10b19f6f15100d4e12209fec68ccb9a1a157fba2
JCARLOS3009 <engcarlosbarreto@gmail.com>
2023-06-25 19:34:48 -0300

    primeiro commit

---
+++ database/migrations/2023_06_25_151846_create_contas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contas', function (Blueprint $table) {
+            $table->id();
+            $table->string('numero');
+            $table->integer('saldo');
+            $table->string('senha');
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contas');
+    }
+};
